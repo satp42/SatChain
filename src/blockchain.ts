e549f144dc9cad894dcfa81356fd48bd0401cd0c
satp42 <msg4satwik@gmail.com>
2021-06-11 16:48:51 -0700

    genesisBlock + genNextBlock

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -14,5 +14,18 @@ class Block {
     }
 }
 
+const generateNextBlock = (blockData: string) => {
+    const previousBlock: Block = getLatestBlock();
+    const nextIndex: number = previousBlock.index + 1;
+    const nextTimestamp: number = new Date().getTime() / 1000;
+    const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+    return newBlock;
+}
+
+const genesisBlock: Block = new Block(
+    0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', null, 1465154705, 'This is an easter egg'
+);
+
 const calculateHash = (index: number, previousHash:string, timestamp: number, data:string): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
