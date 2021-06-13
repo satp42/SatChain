083c26063111fb865801fd590dcda558fc58e3d5
satp42 <msg4satwik@gmail.com>
2021-06-12 20:09:33 -0700

    express server setup

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,3 +1,5 @@
+import bodyParser from "body-parser";
+
 class Block {
     public index: number;
     public hash: string;
@@ -80,4 +82,29 @@ const replaceChain = (newBlock: Block[]) => {
     } else {
         console.log('Invalid new blockchain');
     }
+};
+
+const initHTTPServer = ( myHTTPPort: number) => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        res.send(getBlockchain());
+    });
+
+    app.get('/mintBlock', (req, res) => {
+        const newBlock: Block => generateNextBlock(req.body.data);
+        res.send(newBlock);
+    });
+    app.get('/peers', (req, res) => {
+        res.send(getSockets().map((s: any) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+    });
+    app.post('/addPeer', (req, res) => {
+        connectToPeers(req.body.peer);
+        res.send();
+    });
+
+    app.listen(myHttpPort, () => {
+        console.log('Listening http on port: ' + myHTTPPort);
+    });
 };
